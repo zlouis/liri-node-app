80fd73a5e25eb495a2e2cd65992ce0dad39d3fbd
Louis Huang <jiannchyao@gmail.com>
2016-10-12 11:40:59 -0400

    Add files via upload

--- liri.js
+++ liri.js
@@ -1,18 +1,61 @@
 
 var getKeys = require('./keys.js');
-
+var twitter = require('twitter');
+var request = require('request');
+var spotify = require('spotify');
+var fs = require('fs');
 console.log("=======");
-console.log(getKeys);
+// console.log(getKeys);
 
+var nodeArgv=process.argv;
+var userCommand= process.argv[2]
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<nodeArgv.length; i++){
+  if(i>3 && i<nodeArgv.length){
+    x = x + "+" + nodeArgv[i];
+  } else{
+    x = x + nodeArgv[i];
+  }
+}
 // console.log(getKeys.twitterKeys);
 
 
+switch (userCommand) {
+  case "my-tweets": 
+        showTweets()
+        break;
 
+  case "spotify-this-song":
+      if (x) {
+      showSpotify(x);
+      } else {
+        spotify("Flourscent Adolescent")
+      }
+      break;
+  case "movie-this":
+      if (x) {
+        showOmdb(x) }
+       else {
+        omdbData("Mr.NoBody")
+      }
+      break;
+
+  case "do-what-it-says":
+        doThing();
+        break;
+
+  default:
+  console.log("Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says");
+  break;
+x`
+
+}
 
 
 ///twitter
-var twitter = require('twitter');
 
+function showTweets() {
 var client = new twitter ({
   consumer_key: 'eIZSRvch65EenpaRcRX7rcbZH',
   consumer_secret: 'hLW7NhGSG3Q8wqs8E1c2ldGmBBVeAbmUN8w4KQ9DiDeB53LBRq',
@@ -29,111 +72,107 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         var date = tweets[i].created_at;
         console.log("zLouisH: " + tweets[i].text + " Created At: " + date.substring(0, 19));
         console.log("-----------------------");
+
+        fs.appendFile("zLouisH: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+
     }
 
-        // console.log(JSON.parse(response.body));
-        // console.log(tweets.created_at)
-        // console.log(tweets.text)
-    // for (var i=0;i<tweets.length;i++) {
    }
    else {
           console.log('Error occurred');
    }
-
-
-  
 });
-
-
-
-
-
-
-
-
-
+}
 
 /////omdb
-var request = require('request');
 
-// var node2=process.argv[];
-var movieName=process.argv[2]; // input name of movie
+
+function showOmdb(movie) {
+var movieName=process.argv[3];
 
 
 
 request('http://www.omdbapi.com/?t='+movieName+'&y=&plot=short&r&tomatoes=true&=json', function (error, response, body) {
 
-  // If the request is successful (i.e. if the response status code is 200)
   if (!error && response.statusCode == 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
+    
     console.log("Movie Title: " + JSON.parse(body)['Title']);
-    console.log("Release date: " + JSON.parse(body)['Year']);
-    console.log("Imdb rating: " + JSON.parse(body)['imdbRating']);
+    console.log("Release Aate: " + JSON.parse(body)['Year']);
+    console.log("Imdb Rating: " + JSON.parse(body)['imdbRating']);
     console.log("Rotton Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
     console.log("Rotton Tomatoe's URL: " + JSON.parse(body)["tomatoURL"]);
     console.log("Language: " + JSON.parse(body)['language']);
     console.log("Plot: " + JSON.parse(body)['Plot']);
     console.log("Actors: " + JSON.parse(body)['Actors']);
 
-    // console.log(JSON.parse(body))
+    //adds to log.txt
+
+    fs.appendFile('log.txt', "Title: " + body.Title);
+    fs.appendFile('log.txt', 'Release date: ' + body.Year);
+    fs.appendFile('log.txt', 'Imdb Rating: ' + body.ImdbRating);
+    fs.appendFile('log.txt', 'Rotton Tomatoes Rating: ' + body.tomatoRating)
+    fs.appendFile('log.txt', 'Rotton Tomatoes URL: ' + body.tomatoURL)
+    fs.appendFile('log.txt', 'Language: ' + body.language);
+    fs.appendFile('log.txt', 'Plot: ' + body.plot);
+    fs.appendFile('log.txt', 'Actors: ' + body.Actors);
 
 }
 
-// else {
-//   console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/")
-// }
+else {
+  console.log("Error occured")
+}
 
+if ( movieName===null) {
+       console.log("-----------------------");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!");
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "-----------------------");
+      fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      fs.appendFile('log.txt', "It's on Netflix!");
+}
+
+
+  
 });
-
+}
 
 
 ///spotify 
 
-// node liri.js spotify-this-song '<song name here>'
-
-// This will show the following information about the song in your terminal/bash window
-
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// if no song is provided then your program will default to
-
-// "The Sign" by Ace of Base
-
-
-
-var spotify = require('spotify');
-
-var songName=process.argv[2]
+function showSpotify(song) {
+var songName=process.argv[3]
 
 spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
 
     if (!err ) {
 
-    //      for(var i = 0; i < data.tracks.items.length; i++){
-    //     var songData = data.tracks.items[i];
-    //     //artist
-    //     console.log("Artist: " + songData.artists[0].name);
-    //     //song name
-    //     console.log("Song: " + songData.name);
-    //     //spotify preview link
-    //     console.log("Preview URL: " + songData.preview_url);
-    //     //album name
-    //     console.log("Album: " + songData.album.name);
-    //     console.log("-----------------------");
+         for(var i = 0; i < data.tracks.items.length; i++){
+        var songData = data.tracks.items[i];
+        //artist
+        console.log("Artist: " + songData.artists[0].name);
+        //song name
+        console.log("Song: " + songData.name);
+        //spotify preview link
+        console.log("Preview URL: " + songData.preview_url);
+        //album name
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------");
 
+        fs.appendFile('log.txt', "Artist: " + songData.artists[0].name)
+        fs.appendFile('log.txt', "Artist: " + songData.name);
+        fs.appendFile('log.txt', "Artist: " + songData.preview_url);
+        fs.appendFile('log.txt', "Artist: " + songData.album.name);
 
-    // }
+    }
   }
- 
-  // console.log(album.name)
-               console.log(data.body)
+  else {
 
-
+    console.log("An Error Occured")
+  }
 
 });
 
-///
+}
